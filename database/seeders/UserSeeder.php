1ef98ae2c43cfd4b4cc368cb6f883b93245214da
warmkatahira <t.katahira@warm.co.jp>
2022-07-14 10:28:11 +0900

    seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -34,49 +34,49 @@ class UserSeeder extends Seeder
             'email' => 'warm1@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 2,
         ]);
         User::create([
             'name' => '大泉　一弘',
             'email' => 'warm2@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 3,
         ]);
         User::create([
             'name' => '木村　康洋',
             'email' => 'warm3@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 4,
         ]);
         User::create([
             'name' => '並木　拓',
             'email' => 'warm4@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 5,
         ]);
         User::create([
             'name' => '村上　裕也',
             'email' => 'warm5@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 6,
         ]);
         User::create([
             'name' => '五十嵐　一之',
             'email' => 'warm6@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 7,
         ]);
         User::create([
             'name' => '村上　克也',
             'email' => 'warm7@warm.co.jp',
             'password' => bcrypt('warm1111'),
             'role_id' => 1,
-            'base_id' => 1,
+            'base_id' => 8,
         ]);
     }
 }
